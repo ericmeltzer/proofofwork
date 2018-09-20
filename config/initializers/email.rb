9a21f7a19d91977d2ea4ca3c05074eb96d4beef8
shanhua <shanhua2011@gmail.com>
2018-09-20 15:06:07 +0800

    test gmail port

--- config/initializers/email.rb
+++ config/initializers/email.rb
@@ -1,6 +1,6 @@
 ActionMailer::Base.smtp_settings = {
   :address => "smtp.gmail.com",
-  :port => 587,
+  :port => 465,
   :domain => "gmail.com",
   :user_name => "prooftest487@gmail.com",
   :password => "testwork",
