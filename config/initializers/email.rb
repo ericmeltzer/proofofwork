313d839e9723337810cfaa74dcd8c573bb63c37a
shanhua <shanhua2011@gmail.com>
2018-09-20 14:58:06 +0800

    change mail account

--- config/initializers/email.rb
+++ config/initializers/email.rb
@@ -2,8 +2,8 @@ ActionMailer::Base.smtp_settings = {
   :address => "smtp.gmail.com",
   :port => 587,
   :domain => "gmail.com",
-  :user_name => "pow@primitive.ventures",
-  :password => "catscatscats1!",
+  :user_name => "prooftest487@gmail.com",
+  :password => "testwork",
   :authentication => "plain",
   :enable_starttls_auto => true
 }
