fc4f24063c58998286e872dfda8d8c4597189e60
shanhua <shanhua2011@gmail.com>
2018-09-22 20:52:46 +0800

    change name of mail

--- app/mailers/invitation_mailer.rb
+++ app/mailers/invitation_mailer.rb
@@ -1,5 +1,5 @@
 class InvitationMailer < ActionMailer::Base
-  default :from => "Proofofwork <shanhua2011@gmail.com>"
+  default :from => "Proofofwork <pow@primitive.ventures>"
 
   def invitation(invitation)
 
@@ -7,8 +7,8 @@ class InvitationMailer < ActionMailer::Base
 logger.info "========== invitation: " 
     mail(
       to: invitation.email,
-      subject: "Proofofwork] You are invited to join " <<
-               "Proofofwork"
+      subject: "[Proof of Work] You are invited to join " <<
+               "Proof of Work"
     )
   end
 end
